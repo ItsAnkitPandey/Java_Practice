9047bc9a795112ea81eaf051407430c02a739420
Ankit Pandey <ankitpandey.272003@gmail.com>
2024-03-11 17:51:16 +0530

    initial commit

---
+++ sampleMultiplication.java
@@ -0,0 +1,14 @@
+class sampleMultiplication{
+	public static void main (String[] args){
+		int a = 10;
+		int b = 3;
+
+		System.out.println("a + b = "+ (a+b));
+		System.out.println("a - b = "+ (a-b));
+		System.out.println("a * b = "+ (a*b));
+		System.out.println("a / b = "+ (a/b));
+		System.out.println("a % b = "+ (a%b));
+
+
+}
+}
